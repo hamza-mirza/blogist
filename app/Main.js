d5126ead09759e47e20dc084d82d06cb8582829c
Hamza Shahid <hammzzeeee@gmail.com>
2021-02-23 22:43:12 +0000

    new changes

---
+++ app/Main.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Header from './components/Header'
+import Guest from './components/Guest'
+import About from './components/About'
+import Terms from './components/Terms'
+import Footer from './components/Footer'
+
+function ExampleComponent() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Switch>
+        <Route path='/' exact>
+          <Guest/>
+        </Route>
+        <Route path='/about'>
+          <About/>
+        </Route>
+        <Route path='/terms'>
+          <Terms/>
+        </Route>
+      </Switch>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<ExampleComponent />, document.querySelector("#app"))
+
+if (module.hot) {
+  module.hot.accept()
+}
