e01cec317600d5e7b9a8261fa658559340bfe248
Hamza Shahid <hammzzeeee@gmail.com>
2021-02-25 20:43:35 +0000

    backend connected

--- app/components/Guest.js
+++ app/components/Guest.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Axios from 'axios'
+
 import Page from './Page'
 
 function Guest() {
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      await Axios.post('http://localhost:8080/register', { 
+      username: 'test2', email: 'tesst@tests.com', password: 'qwertssyqwerty'
+    })
+    console.log('Success!') 
+    } catch(e) {
+      console.log(e)
+    }
+  }
   return (
     <Page wide title='Welcome'>
       <div className="row align-items-center">
@@ -10,32 +23,32 @@ function Guest() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
       <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="username-register" className="text-muted mb-1">
               <small>Username</small>
             </label>
             <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email-register" className="text-muted mb-1">
-              <small>Email</small>
-            </label>
-            <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password-register" className="text-muted mb-1">
-              <small>Password</small>
-            </label>
-            <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
-          </div>
-          <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
-            Sign up for Blogist
-          </button>
-        </form>
+           </div>
+            <div className="form-group">
+              <label htmlFor="email-register" className="text-muted mb-1">
+                <small>Email</small>
+              </label>
+             <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+            </div>
+           <div className="form-group">
+             <label htmlFor="password-register" className="text-muted mb-1">
+               <small>Password</small>
+             </label>
+              <input id="password-register" name="password" className="form-control"      type="password" placeholder="Create a password" />
+            </div>
+            <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
+              Sign up for Blogist
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-  </Page>
+    </Page>
   )
 }
 
