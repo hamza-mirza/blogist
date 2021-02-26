9ff6b7770f6094b3d4de519ed38168d576bbdc99
Hamza Shahid <hammzzeeee@gmail.com>
2021-02-26 11:15:30 +0000

    home page feed added

--- app/Main.js
+++ app/Main.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import Header from './components/Header'
 import Guest from './components/Guest'
+import Home from './components/Home'
 import About from './components/About'
 import Terms from './components/Terms'
 import Footer from './components/Footer'
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('blogToken')))
   return (
     <BrowserRouter>
-      <Header/>
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
       <Switch>
         <Route path='/' exact>
-          <Guest/>
+          { loggedIn ? <Home/> : <Guest/> }
         </Route>
         <Route path='/about'>
           <About/>
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,20 +1,19 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
 import HeaderLoggedOut from './HeaderLoggedOut'
 import HeaderLoggedIn from './HeaderLoggedIn'
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('blogToken')))
+function Header(props) {
   return (
-    <header className="header-bar bg-primary mb-3">
+    <header className="header-bar mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
         <h4 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
             Blogist
           </Link>
         </h4>
-        { loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn}/> : <HeaderLoggedOut setLoggedIn={setLoggedIn}/> }
+        { props.loggedIn ? <HeaderLoggedIn setLoggedIn={props.setLoggedIn}/> : <HeaderLoggedOut setLoggedIn={props.setLoggedIn}/> }
       </div>
     </header>
   )
