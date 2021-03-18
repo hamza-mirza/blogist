904362d5de848744223b0e98d4089db170587871
Hamza Shahid <hammzzeeee@gmail.com>
2021-03-18 12:32:18 +0000

    using immer to mutate state

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useReducer } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { useImmerReducer } from 'use-immer'
 import Axios from 'axios'
 Axios.defaults.baseURL = 'http://localhost:8080'
 import Header from './components/Header'
@@ -21,20 +22,20 @@ function Main() {
     loggedIn: Boolean(localStorage.getItem('blogToken')),
     flashMessages: [],
   }
-  function appReducer(state, action) {
+  function appReducer(draft, action) {
     switch (action.type) {
       case 'login':
-        return { loggedIn: true, flashMessages: state.flashMessages }
+        draft.loggedIn = true
+        return
       case 'logout':
-        return { loggedIn: false, flashMessages: state.flashMessages }
+        draft.loggedIn = false
+        return
       case 'flashMessage':
-        return {
-          loggedIn: state.loggedIn,
-          flashMessages: state.flashMessages.concat(action.value),
-        }
+        draft.flashMessages.push(action.value)
+        return
     }
   }
-  const [state, dispatch] = useReducer(appReducer, initialState)
+  const [state, dispatch] = useImmerReducer(appReducer, initialState)
 
   return (
     <StateContext.Provider value={state}>
